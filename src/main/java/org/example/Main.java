6f74ed42e500b4fa90b8427923b0cfe850c1a6db
utkerdogan <utukerdogan@gmail.com>
2025-05-06 02:08:08 +0300

    first tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,54 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+
+        if (isBarking && (clock < 8 || clock > 20)) {
+            return true;
+        }
+
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if((firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if(isSummer){
+            if(temp >= 25 && temp <= 45){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if(temp >= 25 && temp <= 35){
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if(width < 0 || height < 0){
+            return -1;
+        } else {
+            return width * height;
+        }
     }
 
     public static double area(double radius) {
-       
+        if(radius < 0){
+            return -1;
+        } else {
+            return radius * radius * Math.PI;
+        }
     }
 }
